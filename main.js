4346564000b1e3336ea43cd548ab190b4c1b2e5c
Mr Vertical <114293852+kumisaku@users.noreply.github.com>
2025-05-02 18:40:21 +0700

    update#1

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 
+const tetrominoSequence = []
+
+const tetrominos = {
+    'I':[
+        [0,0,0,0],
+        [1,1,1,1],
+        [0,0,0,0],
+        [0,0,0,0]
+    ]
+}
+
+// set the tetromino position
 // https://stackoverflow.com/a/1527820/2124254
 function generatePosition(min, max){
     min = Math.ceil(min);
@@ -7,12 +19,24 @@ function generatePosition(min, max){
     return Math.floor(Math.random() * (max-min + 1)) + min;
 }
 
+
+// generate tetromino block
 // https://tetris.fandom.com/wiki/Random_Generator
 function generateBlock(){
     const sequence = ['I','J','L','O','S','T','Z'];
 
     while(sequence.length){
-        const rad = generatePosition(0,sequence.length-1);
+        const rand = generatePosition(0,sequence.length-1);
         const name = sequence.splice(rand,1)[0];
+        tetrominoSequence.push(name);
     }
+}
+
+function getTetromino(){
+    if(tetrominoSequence.length == 0){
+        generateBlock();
+    }
+
+    const name = tetrominoSequence.pop();
+    const matrix = tetrominos[name];
 }
